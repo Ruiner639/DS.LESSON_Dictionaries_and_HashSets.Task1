7895eed70f7c516a896a99accdef000f66ca529d
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2021-01-30 22:20:21 +0200

    update

--- DS.LESSON_Dictionaries_and_HashSets.Task1/Program.cs
+++ DS.LESSON_Dictionaries_and_HashSets.Task1/Program.cs
@@ -6,6 +6,7 @@ namespace DS.LESSON_Dictionaries_and_HashSets.Task1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("array = {3, 4, 4, 2, 3, 3, 4, 3, 2}");
             string str = "3, 4, 4, 2, 3, 3, 4, 3, 2";
             string[] list = str.Split(", ");
             int[] count = new int[10];
@@ -16,9 +17,9 @@ namespace DS.LESSON_Dictionaries_and_HashSets.Task1
                 count[b]++;
                 a++;
             }
-            Console.WriteLine(count[2]);
-            Console.WriteLine(count[3]);
-            Console.WriteLine(count[4]);
+            Console.WriteLine("2 -> " + count[2] + " times");
+            Console.WriteLine("3 -> " + count[3] + " times");
+            Console.WriteLine("4 -> " + count[4] + " times");
         }
     }
 }
